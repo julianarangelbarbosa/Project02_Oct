411d39b7ce6ca0d708f9f46f873581099a04c642
julianarangelbarbosa <juliana.rangel.barbosa@gmail.com>
2022-11-09 14:28:26 +0000

    Day3Tarde

--- routes/planner.routes.js
+++ routes/planner.routes.js
@@ -26,7 +26,8 @@ router.post('/create', async (req, res, next) => {
 
     //Add task to the user
     await User.findByIdAndUpdate(userId, {$push : {planner: createdTask._id}})
-
+   
+   
     res.redirect(`/planner`);
   } catch (error) {
   console.log(error);
@@ -41,6 +42,7 @@ router.get("/task/:id", async (req, res) => {
   const oneTask = await Task.findById(id)
   res.render("task", oneTask );
 })
+
 
 //See edit task form
 router.get("/edit-task/:id", async (req, res) => {
@@ -67,16 +69,80 @@ router.post('/edit-task/:id', async (req, res, next) => {
     }
   })
 
-/*router.post('/task-delete/:id', async (req, res, next) => {
+//delete task
+router.post('/task-delete/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
+    
     await Task.findByIdAndRemove(id);
     res.redirect('/planner');
   } catch (error) {
     console.log(error);
     next(error);
     }
-  });*/
+  });
+
+
+
+
+
+
+
+
+
+//See edit profile form
+router.get("/edit-profile/:username", async (req, res) => {
+
+  try {
+    const {username} = req.params
+    const userUpdate = await User.find()
+    console.log(userUpdate.username)
+    res.render("edit-profile", {userUpdate, username});
+  } catch (error) { 
+    console.log(error)
+  }});
+
+//Receives edit profile form 
+router.post('/edit-profile/:username', async (req, res, next) => {
+  try {
+    const { username } = req.params; 
+    const currentUser = req.session.currentUser
+    const { email, address } = req.body
+          
+    const userUpdateAgain = await User.findOneAndUpdate({username: username}, { username, email, address });
+    res.redirect('/profile');
+      
+  } catch (error){
+      console.log(error)
+      next(error);
+    }
+  })
+
+
+ /*  const { id } = req.params; 
+  const { title, description, date, type, status, address } = req.body
+        
+  const updatedTask = await Task.findByIdAndUpdate(id, { title, description, date, type, status, address });
+  res.redirect(`/planner`); */
+    
+
+  /* router.post("/profile/:username/edit-profile", fileUploader.single('profilepicture'), (req, res, next) => {
+    const {username} = req.params;
+    const {location, email, website, linkedin, instagram, bio, password, name, surname, campus, course } = req.body;
+    if(username !== req.session.user.username) {
+      return;
+    } else {
+      if(req.file) {
+        User.findOneAndUpdate({username: username}, {location, email, website, linkedin, instagram, bio, password, name, surname, campus, course, username, profilepicture: req.file.path})
+        .then(() => res.redirect(`/profile/${username}`))
+        .catch(err => next(err))
+      } else {
+        User.findOneAndUpdate({username: username}, {location, email, website, linkedin, instagram, bio, password, name, surname, campus, course, username})
+        .then(() => res.redirect(`/profile/${username}`))
+        .catch(err => next(err))
+      }
+    }
+  }); */
 
 
 module.exports = router;
